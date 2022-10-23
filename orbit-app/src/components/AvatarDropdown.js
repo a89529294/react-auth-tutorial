e824b32bee2fccf5fe026a1b753c416b0bfc1c25
Albert Chang <a89529294@hotmail.com>
2022-10-23 15:35:20 +0800

    finish Handling Auth State

--- orbit-app/src/components/AvatarDropdown.js
+++ orbit-app/src/components/AvatarDropdown.js
@@ -1,22 +1,11 @@
-import React, {
-  useContext,
-  useEffect,
-  useRef,
-  useState
-} from 'react';
-import {
-  faCaretDown,
-  faSignOutAlt
-} from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { AuthContext } from './../context/AuthContext';
-import defaultAvatar from './../images/defaultAvatar.png';
+import React, { useContext, useEffect, useRef, useState } from "react";
+import { faCaretDown, faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { AuthContext } from "./../context/AuthContext";
+import defaultAvatar from "./../images/defaultAvatar.png";
 
 const DropdownItem = ({ item }) => (
-  <button
-    className="text-gray-700 flex items-center"
-    onClick={item.onClick}
-  >
+  <button className="text-gray-700 flex items-center" onClick={item.onClick}>
     <FontAwesomeIcon icon={item.icon} />
     <p className="ml-2">{item.title}</p>
   </button>
@@ -44,26 +33,23 @@ const AvatarDropdown = () => {
 
   const dropdownItems = [
     {
-      title: 'Log Out',
+      title: "Log Out",
       icon: faSignOutAlt,
-      onClick: auth.logout
-    }
+      onClick: auth.logout,
+    },
   ];
 
-  const handleClick = e => {
+  const handleClick = (e) => {
     if (!node.current.contains(e.target)) {
       setDropdownOpen(false);
     }
   };
 
   useEffect(() => {
-    document.addEventListener('mousedown', handleClick);
+    document.addEventListener("mousedown", handleClick);
 
     return () => {
-      document.removeEventListener(
-        'mousedown',
-        handleClick
-      );
+      document.removeEventListener("mousedown", handleClick);
     };
   }, []);
 
@@ -72,15 +58,14 @@ const AvatarDropdown = () => {
       <button
         ref={node}
         className="flex rounded-full items-center py-2 px-3 bg-gradient focus:outline-none shadow-lg"
-        onClick={() => setDropdownOpen(!dropdownOpen)}
-      >
+        onClick={() => setDropdownOpen(!dropdownOpen)}>
         <img
           src={authState.userInfo.avatar || defaultAvatar}
           className="rounded-full w-6 border-2 border-white"
           alt="Avatar"
         />
         <div className="px-3">
-          <p className="text-white">Ryan</p>
+          <p className="text-white">{authState.userInfo.firstName}</p>
         </div>
         <div className="mr-1 text-white">
           <FontAwesomeIcon icon={faCaretDown} />
--- orbit-app/src/context/AuthContext.js
+++ orbit-app/src/context/AuthContext.js
@@ -1,19 +1,63 @@
-import React, { createContext } from 'react';
+import React, { createContext, useState } from "react";
+import { useHistory } from "react-router";
 
 const AuthContext = createContext();
 const { Provider } = AuthContext;
 
 const AuthProvider = ({ children }) => {
+  const history = useHistory();
+  const [authState, setAuthState] = useState(() => {
+    return {
+      token: localStorage.getItem("token") ?? null,
+      expiresAt: +localStorage.getItem("expiresAt") ?? null,
+      userInfo: JSON.parse(localStorage.getItem("userInfo")) ?? {},
+    };
+  });
+
+  function setAuthInfo({ token, expiresAt, userInfo }) {
+    localStorage.setItem("token", token);
+    localStorage.setItem("userInfo", JSON.stringify(userInfo));
+    localStorage.setItem("expiresAt", expiresAt);
+    setAuthState({
+      token,
+      expiresAt,
+      userInfo,
+    });
+  }
+
+  function isAuthenticated() {
+    if (!authState.token || !authState.expiresAt) return false;
+    return authState.expiresAt * 1000 > Date.now();
+  }
+
+  function isAdmin() {
+    if (!authState.token || !authState.expiresAt) return false;
+    return authState.userInfo.role === "admin";
+  }
+
+  function logout() {
+    localStorage.removeItem("token");
+    localStorage.removeItem("expiresAt");
+    localStorage.removeItem("userInfo");
+
+    setAuthState({
+      token: null,
+      expiresAt: null,
+      userInfo: {},
+    });
+
+    history.push("/login");
+  }
+
   return (
     <Provider
       value={{
-        authState: {
-          token: null,
-          expiresAt: null,
-          userInfo: {}
-        }
-      }}
-    >
+        authState,
+        setAuthState: (authInfo) => setAuthInfo(authInfo),
+        isAuthenticated,
+        logout,
+        isAdmin,
+      }}>
       {children}
     </Provider>
   );
